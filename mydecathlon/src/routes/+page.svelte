373f4ea1e8b1fe11efff21a933c13d8b5841752c
BERTHORELLY Lea <73932490+Lea-BERTHORELLY@users.noreply.github.com>
2023-01-09 10:41:01 +0100

    Gestion du changement de contenu au clic sur le menu

--- mydecathlon/src/routes/+page.svelte
+++ mydecathlon/src/routes/+page.svelte
@@ -29,7 +29,7 @@
 		}
 	}	
 	import MediaQuery from "./MediaQuery.svelte";
-	import {categorie, csp_med, csp_moins, csp_plus, entre_16_25_ans, entre_26_45_ans, femme, homme, moins_15_ans, plus_46, products} from "$lib/store";
+	import {categorie, csp_med, csp_moins, csp_plus, entre_16_25_ans, entre_26_45_ans, femme, homme, moins_15_ans, plus_46, products, sport} from "$lib/store";
 	import {cptVar} from "$lib/store";
 	export async function load() {
         return {
@@ -55,6 +55,8 @@
 	plus_46.subscribe($ => $plus_46)()
 	homme.subscribe($ => $homme)()
 	femme.subscribe($ => $femme)()
+
+	sport.subscribe($ => $sport)()
 	
 	
 	$: variable=$csp_moins+""+$csp_med+""+$csp_plus+""+$moins_15_ans+""+$entre_16_25_ans+""+$entre_26_45_ans+""+$plus_46+""+$homme+""+$femme
@@ -115,9 +117,6 @@
 	{/if}
 </MediaQuery>
 
-	  <!-- Add some margin-left to this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page) -->
-	  <!-- <div id="main" class:pushMainToRight={navOpen}> -->
-
 	<div class="w3-content w3-padding" style="max-width:1564px">
 
 		<div class="w3-container w3-content w3-center w3-padding-32" id="products">
@@ -125,29 +124,83 @@
 		</div>	
 			<p style="text-align:center">{variable}</p>
 			<div class="w3-row-padding ">
-				<!-- svelte-ignore a11y-missing-attribute -->
-				{#if $cptVar < 3}
-				{#each products as product}
-				<div class="w3-third w3-container w3-margin-bottom">
-						<a on:click={() => { variable = ((product.csp_moins+$csp_moins)+""+(product.csp_med+$csp_med)+""+(product.csp_plus+$csp_plus)+""+(product.moins_15_ans+$moins_15_ans)+""+(product.entre_16_25_ans+$entre_16_25_ans)+""+(product.entre_26_45_ans+$entre_26_45_ans)+""+(product.plus_46+$plus_46)+""+(product.homme+$homme)+""+(product.femme+$femme)), csp_moins.update(csp_moins=>product.csp_moins+csp_moins),csp_med.update(csp_med=>product.csp_med+csp_med),csp_plus.update(csp_plus=> product.csp_plus+csp_plus),moins_15_ans.update(moins_15_ans => product.moins_15_ans+moins_15_ans),entre_16_25_ans.update(entre_16_25_ans => product.entre_16_25_ans+entre_16_25_ans),entre_26_45_ans.update(entre_26_45_ans => product.entre_26_45_ans+entre_26_45_ans),plus_46.update(plus_46 => product.plus_46+plus_46),homme.update(homme => product.homme+homme),femme.update(femme=>product.femme+femme),cptVar.update(cptVar => cptVar+ 1);}}><img src={product.image} width=400 height=auto style="width:100%"> </a>
-						<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
-						<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
-						<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
-				</div>
-				{/each}
-					
-				{:else}
-				<!-- {moyenne()}  -->
-				{#each products as product}
-					{#if (Math.abs((product.csp_moins-$csp_moins))+Math.abs((product.csp_med-$csp_med))+Math.abs((product.csp_plus-$csp_plus))+Math.abs((product.moins_15_ans-$moins_15_ans))+Math.abs((product.entre_16_25_ans-$entre_16_25_ans))+Math.abs((product.entre_26_45_ans-$entre_26_45_ans))+Math.abs((product.plus_46-$plus_46))+Math.abs((product.homme-$homme))+Math.abs((product.femme-$femme)))<=30}
-						<div class="w3-third w3-container w3-margin-bottom">
-							<a href="#"><img src={product.image} width=400 height=auto style="width:100%"> </a>
-							<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
-							<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
-							<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
-						</div>
+
+				{#if $categorie==""} <!-- Si la catégorie n'est pas renseignée: aka on est sur la page d'accueil ou sur la page d'un sport -->
+					{#if $cptVar < 3}
+						{#if $sport==""} <!-- Si le sport n'est pas renseigné: aka on est sur la page d'accueil -->
+							{#each products as product}
+								<div class="w3-third w3-container w3-margin-bottom">
+										<a on:click={() => { variable = ((product.csp_moins+$csp_moins)+""+(product.csp_med+$csp_med)+""+(product.csp_plus+$csp_plus)+""+(product.moins_15_ans+$moins_15_ans)+""+(product.entre_16_25_ans+$entre_16_25_ans)+""+(product.entre_26_45_ans+$entre_26_45_ans)+""+(product.plus_46+$plus_46)+""+(product.homme+$homme)+""+(product.femme+$femme)), csp_moins.update(csp_moins=>product.csp_moins+csp_moins),csp_med.update(csp_med=>product.csp_med+csp_med),csp_plus.update(csp_plus=> product.csp_plus+csp_plus),moins_15_ans.update(moins_15_ans => product.moins_15_ans+moins_15_ans),entre_16_25_ans.update(entre_16_25_ans => product.entre_16_25_ans+entre_16_25_ans),entre_26_45_ans.update(entre_26_45_ans => product.entre_26_45_ans+entre_26_45_ans),plus_46.update(plus_46 => product.plus_46+plus_46),homme.update(homme => product.homme+homme),femme.update(femme=>product.femme+femme),cptVar.update(cptVar => cptVar+ 1);}}><img src={product.image} width=400 height=auto style="width:100%"> </a>
+										<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
+										<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
+										<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
+								</div>
+							{/each}
+						{:else}
+							{#each products as product}
+								{#if product.sport==$sport}
+									<div class="w3-third w3-container w3-margin-bottom">
+										<a on:click={() => { variable = ((product.csp_moins+$csp_moins)+""+(product.csp_med+$csp_med)+""+(product.csp_plus+$csp_plus)+""+(product.moins_15_ans+$moins_15_ans)+""+(product.entre_16_25_ans+$entre_16_25_ans)+""+(product.entre_26_45_ans+$entre_26_45_ans)+""+(product.plus_46+$plus_46)+""+(product.homme+$homme)+""+(product.femme+$femme)), csp_moins.update(csp_moins=>product.csp_moins+csp_moins),csp_med.update(csp_med=>product.csp_med+csp_med),csp_plus.update(csp_plus=> product.csp_plus+csp_plus),moins_15_ans.update(moins_15_ans => product.moins_15_ans+moins_15_ans),entre_16_25_ans.update(entre_16_25_ans => product.entre_16_25_ans+entre_16_25_ans),entre_26_45_ans.update(entre_26_45_ans => product.entre_26_45_ans+entre_26_45_ans),plus_46.update(plus_46 => product.plus_46+plus_46),homme.update(homme => product.homme+homme),femme.update(femme=>product.femme+femme),cptVar.update(cptVar => cptVar+ 1);}}><img src={product.image} width=400 height=auto style="width:100%"> </a>
+										<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
+										<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
+										<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
+									</div>
+								{/if}
+							{/each}				
+						{/if}		
+					{:else} <!-- On a déjà cliqué sur 3 produits, on affiche les produits qui correspondent à la variable -->
+						{#if $sport==""}
+							{#each products as product}
+								{#if (Math.abs((product.csp_moins-$csp_moins))+Math.abs((product.csp_med-$csp_med))+Math.abs((product.csp_plus-$csp_plus))+Math.abs((product.moins_15_ans-$moins_15_ans))+Math.abs((product.entre_16_25_ans-$entre_16_25_ans))+Math.abs((product.entre_26_45_ans-$entre_26_45_ans))+Math.abs((product.plus_46-$plus_46))+Math.abs((product.homme-$homme))+Math.abs((product.femme-$femme)))<=30}
+									<div class="w3-third w3-container w3-margin-bottom">
+										<a href="#"><img src={product.image} width=400 height=auto style="width:100%"> </a>
+										<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
+										<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
+										<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
+									</div>
+								{/if}
+							{/each}
+						{:else}
+							{#each products as product}
+								{#if product.sport==$sport}
+									{#if (Math.abs((product.csp_moins-$csp_moins))+Math.abs((product.csp_med-$csp_med))+Math.abs((product.csp_plus-$csp_plus))+Math.abs((product.moins_15_ans-$moins_15_ans))+Math.abs((product.entre_16_25_ans-$entre_16_25_ans))+Math.abs((product.entre_26_45_ans-$entre_26_45_ans))+Math.abs((product.plus_46-$plus_46))+Math.abs((product.homme-$homme))+Math.abs((product.femme-$femme)))<=30}
+										<div class="w3-third w3-container w3-margin-bottom">
+											<a href="#"><img src={product.image} width=400 height=auto style="width:100%"> </a>
+											<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
+											<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
+											<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
+										</div>
+									{/if}
+								{/if}
+							{/each}
+						{/if}
 					{/if}
-				{/each}
+				{:else} 
+					{#if $cptVar < 3} 
+						{#each products as product}
+							{#if product.categorie==$categorie}
+								<div class="w3-third w3-container w3-margin-bottom">
+									<a on:click={() => { variable = ((product.csp_moins+$csp_moins)+""+(product.csp_med+$csp_med)+""+(product.csp_plus+$csp_plus)+""+(product.moins_15_ans+$moins_15_ans)+""+(product.entre_16_25_ans+$entre_16_25_ans)+""+(product.entre_26_45_ans+$entre_26_45_ans)+""+(product.plus_46+$plus_46)+""+(product.homme+$homme)+""+(product.femme+$femme)), csp_moins.update(csp_moins=>product.csp_moins+csp_moins),csp_med.update(csp_med=>product.csp_med+csp_med),csp_plus.update(csp_plus=> product.csp_plus+csp_plus),moins_15_ans.update(moins_15_ans => product.moins_15_ans+moins_15_ans),entre_16_25_ans.update(entre_16_25_ans => product.entre_16_25_ans+entre_16_25_ans),entre_26_45_ans.update(entre_26_45_ans => product.entre_26_45_ans+entre_26_45_ans),plus_46.update(plus_46 => product.plus_46+plus_46),homme.update(homme => product.homme+homme),femme.update(femme=>product.femme+femme),cptVar.update(cptVar => cptVar+ 1);}}><img src={product.image} width=400 height=auto style="width:100%"> </a>
+									<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
+									<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
+									<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
+								</div>
+							{/if}
+						{/each}
+					{:else} 
+						{#each products as product}
+							{#if product.categorie==$categorie} 
+								{#if (Math.abs((product.csp_moins-$csp_moins))+Math.abs((product.csp_med-$csp_med))+Math.abs((product.csp_plus-$csp_plus))+Math.abs((product.moins_15_ans-$moins_15_ans))+Math.abs((product.entre_16_25_ans-$entre_16_25_ans))+Math.abs((product.entre_26_45_ans-$entre_26_45_ans))+Math.abs((product.plus_46-$plus_46))+Math.abs((product.homme-$homme))+Math.abs((product.femme-$femme)))<=30}
+									<div class="w3-third w3-container w3-margin-bottom">
+										<a href="#"><img src={product.image} width=400 height=auto style="width:100%"> </a>
+										<p style="font-size: smaller;">{product.name}<br><b>{product.price}</b></p>	
+										<p>{product.csp_moins} {product.csp_med} {product.csp_plus} {product.moins_15_ans} {product.entre_16_25_ans} {product.entre_26_45_ans} {product.plus_46} {product.homme} {product.femme}</p>
+										<p><button class="w3-button w3-block" style="color: white;background-color: #1083b8;">Ajouter au panier</button></p>
+									</div>
+								{/if}
+							{/if}
+						{/each}
+					{/if}
 				{/if}
 			</div>
 		</div>
