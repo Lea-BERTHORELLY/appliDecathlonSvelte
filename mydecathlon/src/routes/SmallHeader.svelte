55f661d25da770e29cc3a629ec6c41da6b649683
BERTHORELLY Lea <73932490+Lea-BERTHORELLY@users.noreply.github.com>
2023-01-11 22:03:23 +0100

    Menu smartphone fini

--- mydecathlon/src/routes/SmallHeader.svelte
+++ mydecathlon/src/routes/SmallHeader.svelte
@@ -31,15 +31,58 @@ let navOpen = false;
     import { categorie, sport } from '$lib/store';
     categorie.subscribe($ => $categorie)()
     sport.subscribe($ => $sport)()
+
+    let subNavOpen = false;
+
+    function handleSubNav() {
+        subNavOpen = !subNavOpen;
+    }
 </script>
 
-<div id="mySidenav" class="sidenav" class:open={navOpen}>
+<div id="mySidenav" class="sidenav" class:open={navOpen} style="overflow-y: scroll; max-height: 700px;">
     <a href="#a" class="closebtn" on:click={handleNav}>&times;</a>
     <a class="active" on:click={() => {categorie.update(categorie => categorie="" ), sport.update(sport => sport="");}}>Tous nos produits</a>
     <a  on:click={() => {categorie.update(categorie => categorie="homme" );}}>Homme</a>
     <a on:click={() => {categorie.update(categorie => categorie="femme" );}}>Femme</a>
     <a on:click={() => {categorie.update(categorie => categorie="enfant" );}}>Enfant</a>
+    <a on:click={handleSubNav}>Sports <i class="fa fa-caret-down"></i></a>
+    {#if subNavOpen}
+    <ul>
+        <p>A</p>
+        <li on:click={() => {sport.update(sport => sport="Alpinisme" );}}>Alpinisme</li>
+        <li on:click={() => {sport.update(sport => sport="Apnée" );}}>Apnée</li>
+        <li on:click={() => {sport.update(sport => sport="Aquagym" );}}>Aquagym</li>
+        <li on:click={() => {sport.update(sport => sport="Arts du cirque" );}}>Arts du cirque</li>
+        <li on:click={() => {sport.update(sport => sport="Athlétisme" );}}>Athlétisme</li>
+        <p>B</p>
+        <li on:click={() => {sport.update(sport => sport="Baby foot" );}}>Baby foot</li>
+        <li on:click={() => {sport.update(sport => sport="Baby gym" );}}>Baby gym</li>
+        <li on:click={() => {sport.update(sport => sport="Badminton" );}}>Badminton</li>
+        <li on:click={() => {sport.update(sport => sport="Baseball" );}}>Baseball</li>
+        <li on:click={() => {sport.update(sport => sport="Basketball" );}}>Basketball</li>
+        <p>C</p>
+        <li on:click={() => {sport.update(sport => sport="Canoe kayak" );}}>Canoë kayak</li> 
+        <li on:click={() => {sport.update(sport => sport="Cerf volants" );}}>Cerf volants</li>
+        <li on:click={() => {sport.update(sport => sport="Chasse pêche sous-marine" );}}>Chasse pêche sous-marine</li>
+        <p>D</p>
+        <li on:click={() => {sport.update(sport => sport="Danses" );}}>Danses</li>
+        <p>E</p>
+        <li on:click={() => {sport.update(sport => sport="Escalade" );}}>Escalade</li>
+        <li on:click={() => {sport.update(sport => sport="Escrime" );}}>Escrime</li>
+        <li on:click={() => {sport.update(sport => sport="Equitation" );}}>Equitation</li>
+        <p>F</p>
+        <li on:click={() => {sport.update(sport => sport="Fitness" );}}>Fitness</li>
+        <li on:click={() => {sport.update(sport => sport="Fléchettes" );}}>Fléchettes</li>
+        <li on:click={() => {sport.update(sport => sport="Floorball" );}}>Floorball</li>
+        <p>G</p>
+        <li on:click={() => {sport.update(sport => sport="Golf" );}}>Golf</li>
+        <li on:click={() => {sport.update(sport => sport="Gymnastique artistique et rythmique" );}}>Gymnastique artistique et rythmique</li>
+        <li on:click={() => {sport.update(sport => sport="CGymnastique éducative et sportive" );}}>Gymnastique éducative et sportive</li>  
+              
+    </ul>
+    {/if}
 </div>
+
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <span on:click={handleNav}></span>
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -117,7 +160,12 @@ let navOpen = false;
 	
 .open {
 	width: 40%;
-}	
+}
+
+li,p{
+    list-style-type: none;
+    color: #818181;
+}
 
 /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
 @media screen and (max-height: 450px) {
